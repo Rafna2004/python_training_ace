65a30c1a387794892efa8d3bf361612079b871a8
Rafna <rafnarafeek2004@gmail.com>
2026-08-04 11:18:24 +0530

    Add implementation of a search function for linked list

--- day6/lin.py
+++ day6/lin.py
@@ -14,10 +14,26 @@ a.next = b
 b.next = c
 c.next = d
 
-# Traverse and print the Linked List
+
 temp = head
 while temp:
     print(temp.val, end=" -> ")
     temp = temp.next
 print("None")
+
+
+def is_present(head, target):
+    temp = head
+    while temp:
+        if temp.val == target:
+            return True
+        temp = temp.next
+    return False
+
+target = int(input("Enter number to search: "))
+if is_present(head, target):
+    print(f"Number {target} is present in the linked list.")
+else:
+    print(f"Number {target} is NOT present in the linked list.")
+
 
