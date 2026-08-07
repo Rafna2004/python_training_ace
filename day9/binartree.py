fc57c00535632bec8a39b88cf5c67251e71c557c
Rafna <rafnarafeek2004@gmail.com>
2026-08-07 11:33:12 +0530

    Add implementation of binary tree with pre-order traversal

---
+++ day9/binartree.py
@@ -0,0 +1,29 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+A=TreeNode(1)
+B=TreeNode(2)
+C=TreeNode(3)
+D=TreeNode(4)
+E=TreeNode(5)
+F=TreeNode(10)
+A.left=B
+A.right=C
+B.left=D
+B.right=E
+C.left=F
+print(A.val)
+print(B.val)
+print(C.val)
+print(D.val)
+print(E.val)
+print(F.val)
+def pre_order(node):
+    if not node:
+        return
+    print(node)
+    pre_order(node.left)
+    pre_order(node.right)
+pre_order(A)    
